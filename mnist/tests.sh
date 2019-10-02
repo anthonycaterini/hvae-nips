a53e54b21ed75ca7a051a7f272ef89baf59488b9
Anthony Caterini <anthony.caterini@gmail.com>
2019-10-02 14:51:33 +0100

    Update tests.sh
    fix bug in running of tests wherein `main.py` was omitted

--- mnist/tests.sh
+++ mnist/tests.sh
@@ -6,70 +6,70 @@
 # flags with the test.
 
 # VAE
-python3 -m vae -s 85547
-python3 -m vae -s 12345
-python3 -m vae -s 98765
+python3 main.py -m vae -s 85547
+python3 main.py -m vae -s 12345
+python3 main.py -m vae -s 98765
 
 # HVAE, K = 1
-python3 -m hvae -K 1 -T fixed -s 85547 -ve false
-python3 -m hvae -K 1 -T fixed -s 12345 -ve false
-python3 -m hvae -K 1 -T none -s 85547 -ve false
-python3 -m hvae -K 1 -T none -s 12345 -ve false
+python3 main.py -m hvae -K 1 -T fixed -s 85547 -ve false
+python3 main.py -m hvae -K 1 -T fixed -s 12345 -ve false
+python3 main.py -m hvae -K 1 -T none -s 85547 -ve false
+python3 main.py -m hvae -K 1 -T none -s 12345 -ve false
 
 # HVAE, K = 5
-python3 -m hvae -K 5 -T free -s 85547 -ve false
-python3 -m hvae -K 5 -T free -s 12345 -ve false
-python3 -m hvae -K 5 -T free -s 85547 -ve true
-python3 -m hvae -K 5 -T free -s 12345 -ve true
-python3 -m hvae -K 5 -T fixed -s 85547 -ve false
-python3 -m hvae -K 5 -T fixed -s 12345 -ve false
-python3 -m hvae -K 5 -T fixed -s 85547 -ve true
-python3 -m hvae -K 5 -T fixed -s 12345 -ve true
-python3 -m hvae -K 5 -T none -s 85547 -ve false
-python3 -m hvae -K 5 -T none -s 12345 -ve false
-python3 -m hvae -K 5 -T none -s 85547 -ve true
-python3 -m hvae -K 5 -T none -s 12345 -ve true
+python3 main.py -m hvae -K 5 -T free -s 85547 -ve false
+python3 main.py -m hvae -K 5 -T free -s 12345 -ve false
+python3 main.py -m hvae -K 5 -T free -s 85547 -ve true
+python3 main.py -m hvae -K 5 -T free -s 12345 -ve true
+python3 main.py -m hvae -K 5 -T fixed -s 85547 -ve false
+python3 main.py -m hvae -K 5 -T fixed -s 12345 -ve false
+python3 main.py -m hvae -K 5 -T fixed -s 85547 -ve true
+python3 main.py -m hvae -K 5 -T fixed -s 12345 -ve true
+python3 main.py -m hvae -K 5 -T none -s 85547 -ve false
+python3 main.py -m hvae -K 5 -T none -s 12345 -ve false
+python3 main.py -m hvae -K 5 -T none -s 85547 -ve true
+python3 main.py -m hvae -K 5 -T none -s 12345 -ve true
 
 # HVAE, K = 10
-python3 -m hvae -K 10 -T free -s 85547 -ve false
-python3 -m hvae -K 10 -T free -s 12345 -ve false
-python3 -m hvae -K 10 -T free -s 98765 -ve false
-python3 -m hvae -K 10 -T free -s 85547 -ve true
-python3 -m hvae -K 10 -T free -s 12345 -ve true
-python3 -m hvae -K 10 -T free -s 98765 -ve true
-python3 -m hvae -K 10 -T fixed -s 85547 -ve false
-python3 -m hvae -K 10 -T fixed -s 12345 -ve false
-python3 -m hvae -K 10 -T fixed -s 85547 -ve true
-python3 -m hvae -K 10 -T fixed -s 12345 -ve true
-python3 -m hvae -K 10 -T none -s 85547 -ve false
-python3 -m hvae -K 10 -T none -s 12345 -ve false
-python3 -m hvae -K 10 -T none -s 85547 -ve true
-python3 -m hvae -K 10 -T none -s 12345 -ve true
+python3 main.py -m hvae -K 10 -T free -s 85547 -ve false
+python3 main.py -m hvae -K 10 -T free -s 12345 -ve false
+python3 main.py -m hvae -K 10 -T free -s 98765 -ve false
+python3 main.py -m hvae -K 10 -T free -s 85547 -ve true
+python3 main.py -m hvae -K 10 -T free -s 12345 -ve true
+python3 main.py -m hvae -K 10 -T free -s 98765 -ve true
+python3 main.py -m hvae -K 10 -T fixed -s 85547 -ve false
+python3 main.py -m hvae -K 10 -T fixed -s 12345 -ve false
+python3 main.py -m hvae -K 10 -T fixed -s 85547 -ve true
+python3 main.py -m hvae -K 10 -T fixed -s 12345 -ve true
+python3 main.py -m hvae -K 10 -T none -s 85547 -ve false
+python3 main.py -m hvae -K 10 -T none -s 12345 -ve false
+python3 main.py -m hvae -K 10 -T none -s 85547 -ve true
+python3 main.py -m hvae -K 10 -T none -s 12345 -ve true
 
 # HVAE, K = 15
-python3 -m hvae -K 15 -T free -s 85547 -ve false
-python3 -m hvae -K 15 -T free -s 12345 -ve false
-python3 -m hvae -K 15 -T free -s 85547 -ve true
-python3 -m hvae -K 15 -T free -s 12345 -ve true
-python3 -m hvae -K 15 -T fixed -s 85547 -ve false
-python3 -m hvae -K 15 -T fixed -s 12345 -ve false
-python3 -m hvae -K 15 -T fixed -s 85547 -ve true
-python3 -m hvae -K 15 -T fixed -s 12345 -ve true
-python3 -m hvae -K 15 -T none -s 85547 -ve false
-python3 -m hvae -K 15 -T none -s 12345 -ve false
-python3 -m hvae -K 15 -T none -s 85547 -ve true
-python3 -m hvae -K 15 -T none -s 12345 -ve true
+python3 main.py -m hvae -K 15 -T free -s 85547 -ve false
+python3 main.py -m hvae -K 15 -T free -s 12345 -ve false
+python3 main.py -m hvae -K 15 -T free -s 85547 -ve true
+python3 main.py -m hvae -K 15 -T free -s 12345 -ve true
+python3 main.py -m hvae -K 15 -T fixed -s 85547 -ve false
+python3 main.py -m hvae -K 15 -T fixed -s 12345 -ve false
+python3 main.py -m hvae -K 15 -T fixed -s 85547 -ve true
+python3 main.py -m hvae -K 15 -T fixed -s 12345 -ve true
+python3 main.py -m hvae -K 15 -T none -s 85547 -ve false
+python3 main.py -m hvae -K 15 -T none -s 12345 -ve false
+python3 main.py -m hvae -K 15 -T none -s 85547 -ve true
+python3 main.py -m hvae -K 15 -T none -s 12345 -ve true
 
 # HVAE, K = 20
-python3 -m hvae -K 20 -T free -s 85547 -ve false
-python3 -m hvae -K 20 -T free -s 12345 -ve false
-python3 -m hvae -K 20 -T free -s 85547 -ve true
-python3 -m hvae -K 20 -T free -s 12345 -ve true
-python3 -m hvae -K 20 -T fixed -s 85547 -ve false
-python3 -m hvae -K 20 -T fixed -s 12345 -ve false
-python3 -m hvae -K 20 -T fixed -s 85547 -ve true
-python3 -m hvae -K 20 -T fixed -s 12345 -ve true
-python3 -m hvae -K 20 -T none -s 85547 -ve false
-python3 -m hvae -K 20 -T none -s 12345 -ve false
-python3 -m hvae -K 20 -T none -s 85547 -ve true
-python3 -m hvae -K 20 -T none -s 12345 -ve true
+python3 main.py -m hvae -K 20 -T free -s 85547 -ve false
+python3 main.py -m hvae -K 20 -T free -s 12345 -ve false
+python3 main.py -m hvae -K 20 -T free -s 85547 -ve true
+python3 main.py -m hvae -K 20 -T free -s 12345 -ve true
+python3 main.py -m hvae -K 20 -T fixed -s 85547 -ve false
+python3 main.py -m hvae -K 20 -T fixed -s 12345 -ve false
+python3 main.py -m hvae -K 20 -T fixed -s 85547 -ve true
+python3 main.py -m hvae -K 20 -T fixed -s 12345 -ve true
+python3 main.py -m hvae -K 20 -T none -s 85547 -ve false
+python3 main.py -m hvae -K 20 -T none -s 12345 -ve false
+python3 main.py -m hvae -K 20 -T none -s 85547 -ve true
+python3 main.py -m hvae -K 20 -T none -s 12345 -ve true
